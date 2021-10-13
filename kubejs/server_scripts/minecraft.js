980dceba22dc4fe58e06bb96cb2546e46cfc40f9
nddragoon <54559451+nddragoon@users.noreply.github.com>
2021-10-13 18:45:38 -0500

    Update minecraft.js

--- kubejs/server_scripts/minecraft.js
+++ kubejs/server_scripts/minecraft.js
@@ -1,3 +1,7 @@
 onEvent('recipes', e => {
+    e.shaped('16x minecraft:stick', ['L', 'L'], {
+        L: '#minecraft:logs'
+    }).id('kubejs:minecraft/shaped/logs_to_sticks')
+
     e.smelting('minecraft:iron_ingot', 'compressium:gravel_1').id('kubejs:minecraft/smelting/iron_ingot')
 })
