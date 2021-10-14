1f50656351e22d0ce2aa864d721e240a36bf9aaa
nddragoon <54559451+nddragoon@users.noreply.github.com>
2021-10-14 14:36:53 -0500

    Update minecraft.js

--- kubejs/server_scripts/minecraft.js
+++ kubejs/server_scripts/minecraft.js
@@ -3,5 +3,12 @@ onEvent('recipes', e => {
         L: '#minecraft:logs'
     }).id('kubejs:minecraft/shaped/logs_to_sticks')
 
+    e.shaped('minecraft:stone_pickaxe', ['SSS', ' R ', ' R '], {
+        S: '#forge:stone',
+        R: '#forge:rods/wooden'
+    }).id('kubejs:minecraft/shaped/stone_pickaxe')
+
+
+
     e.smelting('minecraft:iron_ingot', 'compressium:gravel_1').id('kubejs:minecraft/smelting/iron_ingot')
 })
