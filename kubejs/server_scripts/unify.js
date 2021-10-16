cf163c53c3df7d1964b95b0d8cbe096b60f4997a
nddragoon <54559451+nddragoon@users.noreply.github.com>
2021-10-15 20:16:13 -0500

    Update unify.js

--- kubejs/server_scripts/unify.js
+++ kubejs/server_scripts/unify.js
@@ -1,10 +1,26 @@
 onEvent('recipes', e => {
-    function metals(metal, ingot, dust, block, nugget, plate) {
+    //Explicitly declares each item type for metals that lack one of them in Emendatus Enigmatica, particularly vanilla ones
+    function miscMetals(metal, ingot, dust, block, nugget, plate, gear, rod) {
         e.replaceOutput(`#forge:ingots/${metal}`, ingot)
         e.replaceOutput(`#forge:dusts/${metal}`, dust)
         e.replaceOutput(`#forge:storage_blocks/${metal}`, block)
         e.replaceOutput(`#forge:nuggets/${metal}`, nugget)
         e.replaceOutput(`#forge:plates/${metal}`, plate)
+        e.replaceOutput(`#forge:gears/${metal}`, gear)
+        e.replaceOutput(`#forge:rods/${metal}`, rod)
+    }
+
+    //For metals fully handled by EE
+    function emendatusMetals(metals) {
+        metals.forEach(element => {
+            e.replaceOutput(`#forge:ingots/${element}`, `emendatusenigmatica:${element}_ingot`)
+            e.replaceOutput(`#forge:dusts/${element}`, `emendatusenigmatica:${element}_dust`)
+            e.replaceOutput(`#forge:storage_blocks/${element}`, `emendatusenigmatica:${element}_block`)
+            e.replaceOutput(`#forge:nuggets/${element}`, `emendatusenigmatica:${element}_nugget`)
+            e.replaceOutput(`#forge:plates/${element}`, `emendatusenigmatica:${element}_plate`)
+            e.replaceOutput(`#forge:gears/${element}`, `emendatusenigmatica:${element}_gear`)
+            e.replaceOutput(`#forge:rods/${element}`, `emendatusenigmatica:${element}_rod`)
+        })
     }
 
     function removeByID([recipes]) {
@@ -13,18 +29,49 @@ onEvent('recipes', e => {
         }
     }
 
-    metals('iron', 'minecraft:iron_ingot', 'thermal:iron_dust', 'minecraft:iron_block', 'minecraft:iron_nugget', 'create:iron_sheet')
-    metals('gold', 'minecraft:gold_ingot', 'thermal:gold_dust', 'minecraft:gold_block', 'minecraft:gold_nugget', 'create:golden_sheet')
-    metals('zinc', 'create:zinc_ingot', 'jaopca:dusts.zinc', 'create:zinc_block', 'create:zinc_nugget', 'createaddition:zinc_sheet')
-    metals('uranium', 'mekanism:ingot_uranium', 'mekanism:dust_uranium', 'mekanism:block_uranium', 'mekanism:nugget_uranium', 'immersiveengineering:plate_uranium')
-    metals('copper', 'create:copper_ingot', 'thermal:copper_dust', 'create:copper_block', 'create:copper_nugget', 'create:copper_sheet')
-    metals('aluminum', 'immersiveengineering:ingot_aluminum', 'immersiveengineering:dust_aluminum', 'immersiveengineering:storage_aluminum', 'immersiveengineering:nugget_aluminum', 'immersiveengineering:plate_aluminum')
-    metals('nickel', 'thermal:nickel_ingot', 'thermal:nickel_dust', 'thermal:nickel_block', 'thermal:nickel_nugget', 'thermal:nickel_plate')
-    metals('lead', 'thermal:lead_ingot', 'thermal:lead_dust', 'thermal:lead_block', 'thermal:lead_nugget', 'thermal:lead_plate')
-    metals('silver', 'thermal:silver_ingot', 'thermal:silver_dust', 'thermal:silver_block', 'thermal:silver_nugget', 'thermal:silver_plate')
-    metals('constantan', 'thermal:constantan_ingot', 'thermal:constantan_dust', 'thermal:constantan_block', 'thermal:constantan_nugget', 'thermal:constantan_plate')
-    metals('electrum', 'thermal:electrum_ingot', 'thermal:electrum_dust', 'thermal:electrum_block', 'thermal:electrum_nugget', 'thermal:electrum_plate')
-    metals('steel', 'mekanism:ingot_steel', 'mekanism:dust_steel', 'mekanism:block_steel', 'mekanism:nugget_steel', 'immersiveengineering:plate_steel')
-    metals('bronze', 'thermal:bronze_ingot', 'thermal:bronze_dust', 'thermal:bronze_block', 'thermal:bronze_nugget', 'thermal:bronze_plate')
-    metals('tin', 'thermal:tin_ingot', 'thermal:tin_dust', 'thermal:tin_block', 'thermal:tin_nugget', 'thermal:tin_plate')
+
+    miscMetals(
+        'iron',
+        'minecraft:iron_ingot',
+        'emendatusenigmatica:iron_dust',
+        'minecraft:iron_block',
+        'minecraft:iron_nugget',
+        'emendatusenigmatica:iron_plate',
+        'emendatusenigmatica:iron_gear',
+        'emendatusenigmatica:iron_rod'
+    )
+
+    miscMetals(
+        'gold',
+        'minecraft:gold_ingot',
+        'emendatusenigmatica:gold_dust',
+        'minecraft:gold_block',
+        'minecraft:gold_nugget',
+        'emendatusenigmatica:gold_plate',
+        'emendatusenigmatica:gold_gear',
+        'emendatusenigmatica:gold_rod'
+    )
+
+
+    emendatusMetals([
+        'copper',
+        'aluminum',
+        'silver',
+        'lead',
+        'nickel',
+        'uranium',
+        'osmium',
+        'tin',
+        'zinc',
+        'cobalt',
+        'bronze',
+        'brass',
+        'constantan',
+        'electrum',
+        'steel',
+        'invar',
+        'signalum',
+        'lumium',
+        'enderium'
+    ])
 })
